2ca29fefd87ceeae7fc3b5c3de9c6fc39f16d3b0
Ismail Mazumder <im087921@gmail.com>
2025-04-25 15:11:45 +0600

    o

--- main.py
+++ main.py
@@ -69,4 +69,4 @@ def ask():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=9000, debug=True)
+    app.run(host="0.0.0.0", port=9000, debug=True)
